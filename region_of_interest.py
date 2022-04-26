997fc9411c050d7263e57911c6a74484a1f72c0e
ali121299 <85250818+ali121299@users.noreply.github.com>
2022-04-26 16:54:48 +0200

    Add files via upload

---
+++ region_of_interest.py
@@ -0,0 +1,13 @@
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    #channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+region_of_interest_vertices = [
+    (200, 665),
+    (620,410),
+    (1130, 665)
+  ]
