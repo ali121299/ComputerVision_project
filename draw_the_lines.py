7f410ed5896c5f014a91a2ba2398f5a24a231093
ali121299 <85250818+ali121299@users.noreply.github.com>
2022-04-26 16:58:26 +0200

    Add files via upload

---
+++ draw_the_lines.py
@@ -0,0 +1,10 @@
+def drow_the_lines(img, lines):
+    img = np.copy(img)
+    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
+
+    img = cv2.addWeighted(img, 1, blank_image, 1, 0.0)
+    return img, blank_image
