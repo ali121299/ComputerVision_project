093ce58d8c95fdce73dc777e0c9e673b6eeb723f
ali121299 <85250818+ali121299@users.noreply.github.com>
2022-04-26 17:00:09 +0200

    Add files via upload

---
+++ Hough_lines.py
@@ -0,0 +1,8 @@
+lines = cv2.HoughLinesP(cropped_image,
+                        rho=6,
+                        theta=np.pi/180,
+                        threshold=160,
+                        lines=np.array([]),
+                        minLineLength=40,
+                        maxLineGap=25)
+image_with_lines , mask = drow_the_lines(image, lines)
